db57fec96b3acb31a3d95fdfbc5f51a534defb52
Okaroni Purity <nekesapurity20@gmail.com>
2025-08-11 10:13:31 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,26 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    approved_breeds = ["Pug", "Dalmatian", "Mutt", "Bulldog"]
+
+    def __init__(self, name=None, breed=None):
+        self.name = None
+        self.breed = None
+        if name is not None:
+            self.set_name(name)
+        if breed is not None:
+            self.set_breed(breed)
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_breed(self, breed):
+        if breed in Dog.approved_breeds:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    approved_jobs = ["Sales", "Engineer", "Manager", "ITC"]
+
+    def __init__(self, name=None, job=None):
+        self.name = None
+        self.job = None
+        if name is not None:
+            self.set_name(name)
+        if job is not None:
+            self.set_job(job)
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_job(self, job):
+        if job in Person.approved_jobs:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
     pass
